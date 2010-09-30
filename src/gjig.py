1ff04084005669ecda46d7764ff3d880997378df
Tshepang Lekhonkhobe <tshepang@gmail.com>
2010-09-30 22:25:38 +0200

    a small recommendation by python-policy

--- src/gjig.py
+++ src/gjig.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/python
 #
 # Gnome JIG - GNOME interface to wajig sys admin tools
 #
--- src/wajig.py
+++ src/wajig.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 #
 # WAJIG - Debian Command Line System Administrator
 #
