569ea6b745610ac560eec9d2c4b7c2a96fa11824
Tshepang Lekhonkhobe <tshepang@gmail.com>
2012-03-20 00:12:33 +0200

    restore wajig shell; someone requested it

---
+++ src/shell.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#
+# This file is part of wajig.  The copyright file is at debian/copyright.
+
+import subprocess
+import readline
+import os
+import atexit
+import rlcompleter
+
+HISTFILE = os.path.join(os.environ["HOME"], ".wajig", ".wajig-history")
+
+def main():
+
+    try:
+        readline.read_history_file(HISTFILE)
+    except IOError:
+        pass
+    readline.parse_and_bind('tab: complete')
+
+    while True:
+        command_line = input("wajig> ")
+        if command_line in "exit quit bye".split():
+            return
+        command = "wajig " + command_line
+        subprocess.call(command.split())
+
+if __name__ == "__main__":
+    try:
+        main()
+    except EOFError:
+        print()
+    atexit.register(readline.write_history_file, HISTFILE)
--- wajig.sh
+++ wajig.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-/usr/bin/python3 /usr/share/wajig/wajig.py "$@"
+if [ -z "$1" ]; then
+    /usr/share/wajig/shell.py
+else
+    /usr/bin/python3 /usr/share/wajig/wajig.py "$@"
+fi
