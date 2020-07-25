8a3cfd1560bcd15aa41009304976290743853497
Graham Williams <Graham.Williams@togaware.com>
2020-07-26 08:38:22 +1000

    Initial import

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#
+# wajig - GNU/Linux Administration
+#
+# A command line tool for managing the administration of Ubuntu.
+#
+# Copyright (c) Graham.Williams@togaware.com All rights reserved.
+#
+# This file is part of wajig.
+
+# Define application constants.
+
+APP = "wajig"
+VERSION = "3.0.4"
+
+SIMULATE = False
+TEACH = False
